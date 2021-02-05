967280eae5fcc66d7117a766b1c2d5037da92bc0
Fabien Herraud <faherrau@z4r3p3.42lyon.fr>
2021-02-05 09:23:47 +0100

    final update printf headers & update checker source directory

--- display/printf_header/libft_printf_processor.h
+++ display/printf_header/libft_printf_processor.h
@@ -6,7 +6,7 @@
 /*   By: faherrau <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/04 07:47:44 by faherrau          #+#    #+#             */
-/*   Updated: 2021/02/05 08:36:56 by faherrau         ###   ########lyon.fr   */
+/*   Updated: 2021/02/05 09:18:58 by faherrau         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,40 +36,40 @@ size_t	preprocess(
 **	./printf/process/printf_process.c
 */
 size_t	process(
-	t_formated *prtbl,
+	t_formated *formated,
 	va_list args);
 
 /*
 **	Process c type.
 **	./printf/process/processor/printf_c_processor.c
 */
-size_t	c_processor(t_formated *printable, va_list args, char **to_print);
+size_t	c_processor(t_formated *formated, va_list args, char **to_display);
 
 /*
 **	Process s type.
 **	./printf/process/processor/printf_s_processor.c
 */
-void	s_processor(t_formated *printable, va_list args, char **to_print);
+void	s_processor(t_formated *formated, va_list args, char **to_display);
 
 /*
 **	Process p type.
 **	./printf/process/processor/printf_p_processor.c
 */
-void	p_processor(t_formated *printable, va_list args, char **to_print);
+void	p_processor(t_formated *formated, va_list args, char **to_display);
 
 /*
 **	Process d, i, u, x, X types.
 **	./printf/process/processor/printf_diuxx_processor.c
 */
-void	diuxx_processor(t_formated *printable, va_list args, char **to_print);
+void	diuxx_processor(t_formated *formated, va_list args, char **to_display);
 
 /*
 **	Only used by diuxx processor function.
 **	./printf/process/processor/printf_diuxx_processor.c
 */
 t_bool	convert_zeros_and_check_precision(
-	t_formated *prtbl,
-	char **to_print,
+	t_formated *formated,
+	char **to_display,
 	long value);
 
 /*
@@ -77,22 +77,22 @@ t_bool	convert_zeros_and_check_precision(
 **	./printf/process/processor/printf_diuxx_processor.c
 */
 void	fill_zeros(
-	t_formated *prtbl,
-	char **to_print,
+	t_formated *formated,
+	char **to_display,
 	long value);
 
 /*
 **	Process % type.
 **	./printf/process/processor/printf_pcrt_processor.c
 */
-void	pcrt_processor(t_formated *printable, va_list args, char **to_print);
+void	pcrt_processor(t_formated *formated, va_list args, char **to_display);
 
 /*
-**	Display printable.
+**	Display formated.
 **	./printf/process/printf_display.c
 */
 size_t	display(
-	t_formated *prtbl,
-	char **to_print);
+	t_formated *formated,
+	char **to_display);
 
 #endif
